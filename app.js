a4f94d2a19f2ee95d13ba779a894124c3a3f8176
AmanMinhas <amandeepSinghMinhas@gmail.com>
2020-02-06 19:48:46 +0530

    Setup Search Form

---
+++ app.js
@@ -0,0 +1,20 @@
+(function() {
+  /**
+    Happy Path Steps :-
+    1. Collect title and lang from form.
+    2. Call page summary API using lang and title. GET `https://${lang}.wikipedia.org/api/rest_v1/page/summary/${title}`
+    3. Use response.dir to identify page layout direction.
+    4. Use response.api_urls.metadata to make another GET Request.
+    5. Get Table of Content from metadataResponse.toc.entries
+  */
+
+  const formEl = document.querySelector('#search-form');
+  formEl.addEventListener('submit', function(e) {
+      e.preventDefault();
+      handleSearchSubmit();
+  });
+
+  async function handleSearchSubmit() {
+    // TODO
+  }
+})();
