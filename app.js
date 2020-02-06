b9c85fa62858d979da68dab65f864332c63949a1
AmanMinhas <amandeepSinghMinhas@gmail.com>
2020-02-06 21:58:43 +0530

    Setup Dropdown options and Fetch API Data

--- app.js
+++ app.js
@@ -14,7 +14,67 @@
       handleSearchSubmit();
   });
 
+  function getFormValues() {
+    const title = document.querySelector('#article-title').value;
+    const language = document.querySelector('#search-language').value;
+
+    return { title, language };
+  }
+
+  async function getRequest(path) {
+    try {
+      if (!path) throw new Error('Invalid Path');
+
+      let headers = new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'amandeepSinghMinhas@gmail.com',
+        'Api-User-Agent': 'amandeepSinghMinhas@gmail.com'
+      });
+
+      const response = await fetch(path, {
+        method: 'GET',
+        headers
+      });
+      const responseJson = await response.json();
+      if (!response.ok) {
+        const errMsg = responseJson.detail || 'Something went wrong!';
+        throw new Error(errMsg);
+      }
+      return responseJson;
+    } catch (err) {
+      // handle error
+      console.warn({ err });
+      return false;
+    }
+  }
+
+  async function getPageSummary(title, language) {
+    if (!title || !language) return;
+    const path = `https://${language}.wikipedia.org/api/rest_v1/page/summary/${title}`;
+    const summaryRes = await getRequest(path);
+    return summaryRes;
+  }
+
+  async function getPageMetadata(path) {
+    if (!path) return;
+    const metadataRes = await getRequest(path);
+    return metadataRes;
+  }
+
   async function handleSearchSubmit() {
     // TODO
+    const { title, language } = getFormValues();
+
+    // Get Page Summary
+    const pageSummary = await getPageSummary(title, language);
+    console.log({ pageSummary });
+    if (!pageSummary) return;
+
+    // Fetch Metadata
+    const metadataApiPath = pageSummary.api_urls.metadata;
+    const pageMetadata = await getPageMetadata(metadataApiPath);
+    console.log({ pageMetadata });
+    if (!pageMetadata) return;
   }
 })();
